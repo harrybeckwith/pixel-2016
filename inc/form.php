8ece1cdbfb09c67906be5be54e802831e450ab3f
Harry J Beckwith <harryjbeckwith@gmail.com>
2016-12-15 15:59:32 +0000

    required changed for w3c

--- inc/form.php
+++ inc/form.php
@@ -2,17 +2,15 @@
     <div id="contact_results"></div>
     <div id="contact_body">
 
-        <input type="text" name="name" id="name" required="true" placeholder='Name' class="input-field" />
+        <input type="text" name="name" id="name" required placeholder='Name' class="input-field" />
 
+        <input type="email" name="email" required placeholder='Email' class="input-field" />
 
-        <input type="email" name="email" required="true" placeholder='Email' class="input-field" />
+        <input type="text" name="phone1" placeholder="Number" required class="tel-number-field" />
 
+        <span>Message <span class="required">*</span></span>
+        <textarea name="message" id="message" class="textarea-field" required></textarea>
 
-        <input type="text" name="phone1" placeholder="Number" required="true" class="tel-number-field" />
-
-        <label for="field5"><span>Message <span class="required">*</span></span>
-            <textarea name="message" id="message" class="textarea-field" required="true"></textarea>
-        </label>
         <label>
             <span>&nbsp;</span>
             <input type="submit" id="submit_btn" value="Submit" />
