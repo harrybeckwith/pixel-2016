e9a187a34c6fb9de151a86db5b95cf3447bb339b
Harry J Beckwith <harryjbeckwith@gmail.com>
2016-12-15 15:39:44 +0000

    new form

--- inc/form.php
+++ inc/form.php
@@ -1,26 +1,21 @@
-<div id="form-messages"></div>
-<form id="ajax-contact" method="post" action="mailer.php">
-    <div class="field">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-    </div>
+<div class="form-style" id="contact_form">
+    <div id="contact_results"></div>
+    <div id="contact_body">
 
-    <div class="field">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-    </div>
+        <input type="text" name="name" id="name" required="true" placeholder='Name' class="input-field" />
 
-    <div class="field">
-        <label for="email">Number:</label>
-        <input type="number" id="number" name="number" required>
-    </div>
 
-    <div class="field">
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-    </div>
+        <input type="email" name="email" required="true" placeholder='Email' class="input-field" />
 
-    <div class="field">
-        <button type="submit">Send</button>
+
+        <input type="text" name="phone1" placeholder="Number" required="true" class="tel-number-field" />
+
+        <label for="field5"><span>Message <span class="required">*</span></span>
+            <textarea name="message" id="message" class="textarea-field" required="true"></textarea>
+        </label>
+        <label>
+            <span>&nbsp;</span>
+            <input type="submit" id="submit_btn" value="Submit" />
+        </label>
     </div>
-</form>
+</div>
