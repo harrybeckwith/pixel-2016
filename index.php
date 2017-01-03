c8b59b93f9316bb4901469584f9cf556e6ef2c0a
Harry J Beckwith <harryjbeckwith@gmail.com>
2017-01-03 14:57:35 +0000

    Notes on form

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $metaDescription = 'Meta description goes here! change on each page!';
 ?>
     <?php include('inc/head.php'); ?>
         <?php include('inc/header.php'); ?>
+        <!-- TO use form files are in inc/form and to change were the form goes to change mailer.php file-->
             <?php include('inc/form.php'); ?>
                 <?php include('inc/footer.php'); ?>
 
