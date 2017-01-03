3356bf198608ed01a40a2036f81a0cc1faf5b0f4
Harry J Beckwith <harryjbeckwith@gmail.com>
2017-01-03 10:22:23 +0000

    Contact form
    Old form removed and new added with ajax, edit mailer.php to change
    were the email goes.

--- inc/form.php
+++ inc/form.php
@@ -1,19 +1,78 @@
-<div class="form-style" id="contact_form">
-    <div id="contact_results"></div>
-    <div id="contact_body">
+<div id="page-wrapper">
 
-        <input type="text" name="name" id="name" required placeholder='Name' class="input-field" />
+    <div id="form-messages"></div>
 
-        <input type="email" name="email" required placeholder='Email' class="input-field" />
+    <form id="ajax-contact" method="post" action="mailer.php">
+        <div class="field">
+            <input type="text" id="name" name="name" placeholder="Name" required>
+        </div>
 
-        <input type="text" name="phone1" placeholder="Number" required class="tel-number-field" />
+        <div class="field">
+            <input type="email" id="email" name="email" placeholder="Email" required>
+        </div>
 
-        <span>Message <span class="required">*</span></span>
-        <textarea name="message" id="message" class="textarea-field" required></textarea>
+        <div class="field">
+            <input type="number" id="phone" name="phone" placeholder="Phone" required>
+        </div>
 
-        <label>
-            <span>&nbsp;</span>
-            <input type="submit" id="submit_btn" value="Submit" />
-        </label>
-    </div>
+        <div class="field">
+            <textarea id="message" name="message" placeholder="Message" required></textarea>
+        </div>
+
+        <div class="field">
+            <button type="submit">Send</button>
+        </div>
+    </form>
 </div>
+<script>
+    $(function() {
+
+        // Get the form.
+        var form = $('#ajax-contact');
+
+        // Get the messages div.
+        var formMessages = $('#form-messages');
+
+        // Set up an event listener for the contact form.
+        $(form).submit(function(e) {
+            // Stop the browser from submitting the form.
+            e.preventDefault();
+
+            // Serialize the form data.
+            var formData = $(form).serialize();
+
+            // Submit the form using AJAX.
+            $.ajax({
+                    type: 'POST',
+                    url: $(form).attr('action'),
+                    data: formData
+                })
+                .done(function(response) {
+                    // Make sure that the formMessages div has the 'success' class.
+                    $(formMessages).removeClass('error');
+                    $(formMessages).addClass('success');
+
+                    // Set the message text.
+                    $(formMessages).text(response);
+
+                    // Clear the form.
+                    $('#name, #email, #message, #phone').val('');
+                })
+                .fail(function(data) {
+                    // Make sure that the formMessages div has the 'error' class.
+                    $(formMessages).removeClass('success');
+                    $(formMessages).addClass('error');
+
+                    // Set the message text.
+                    if (data.responseText !== '') {
+                        $(formMessages).text(data.responseText);
+                    } else {
+                        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                    }
+                });
+
+        });
+
+    });
+
+</script>
